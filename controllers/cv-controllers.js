b0574a795560bf3018ace6767e8591fb7ecd8a81
Nguyễn Thành Lộc <nguyenthanhloc1308@gmail.com>
2021-05-25 16:25:33 +0700

    Merge Cv

--- controllers/cv-controllers.js
+++ controllers/cv-controllers.js
@@ -1,14 +1,14 @@
 import CV from "../models/Cv-models.js";
-import mongoose from "mongoose";
-
+import User from "../models/User-models.js";
 import Profile from "../models/Profile-models.js";
 import Education from "../models/Education-models.js";
 import Project from "../models/Project-models.js";
 import Experience from "../models/Experience-models.js";
 import Extra from "../models/Extra-models.js";
 
-export const getCv = async (req, res, next) => {
+import mongoose from "mongoose";
 
+export const getCv = async (req, res, next) => {
 }
 
 export const getCvById = async (req, res, next) => {
@@ -18,90 +18,40 @@ export const getCvByUserId = async (req, res, next) => {
 }
 
 export const createCV = async (req, res, next) => {
-  const { userId } = req.body;
-  
+  const { userId } = req.params;
+
   const createdCV = new CV({ userId })
 
+  let user;
   try {
-    await createdCV.save();
-  } catch {
-    return res.status(400).json({ errorMessage: "Some thing went wrong, please try again" });
-  }
-  res.status(201).json({ Message: "Create CV successfull.", user: createdCV.toObject({ getters: true }) });
-}
-
-export const createProfile = async (req, res, next) => {
-  const {
-    cvId,
-    firstname,
-    lastname,
-    dob,
-    email,
-    address,
-    phone
-  } = req.body;
-
-  const createdProfile = new Profile({
-    cvId,
-    firstname,
-    lastname,
-    dob,
-    email,
-    address,
-    phone
-  });
-
-  let cv;
-  try {
-    cv = await CV.findById(cvId);
+    user = await User.findById(userId);
   } catch {
     return res.status(400).json({ errorMessage: "Some thing went wrong, please try again" });
   }
 
-  if (!cv) {
-    return res.status(400).json({ errorMessage: "Can not find this cv, please try again" });
+  if (!user) {
+    return res.status(401).json({ errorMessage: "Can not find this user, please try again" });
   }
 
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
-    await createdProfile.save({ session: sess });
-    cv.profile.push(createdProfile);
-    await cv.save({ sess: sess });
+    createdCV.creator = userId;
+    await createdCV.save({ session: sess });
+    user.cvs.push(createdCV);
+    await user.save({ sess: sess });
     await sess.commitTransaction();
   } catch (err) {
     console.log(err);
-    return res.status(400).json({ errorMessage: "Fail." });
+    return res.status(402).json({ errorMessage: "Fail." });
   }
+  res.status(201).json({ user: createdCV });
+}
 
-  res.status(201).json({ cv: createdProfile });
-};
+export const createProfile = async (req, res, next) => {
+  const { cvId } = req.params;
 
-export const createEducation = async (req, res, next) => {
-  const {
-    cvId,
-    collegeName,
-    collegeMajor,
-    collegeQualification,
-
-    schoolName,
-    schoolMajor,
-    schoolQualification,
-  } = req.body;
-
-  const createdEducation = new Education({
-    cvId,
-    college: {
-      collegeName,
-      collegeMajor,
-      collegeQualification,
-    },
-    school: {
-      schoolName,
-      schoolMajor,
-      schoolQualification,
-    },
-  });
+  const createdProfile = new Profile({ cvId });
 
   let cv;
   try {
@@ -111,7 +61,7 @@ export const createEducation = async (req, res, next) => {
   }
 
   if (!cv) {
-    return res.status(400).json({ errorMessage: "Can not find this cv, please try again" });
+    return res.status(401).json({ errorMessage: "Can not find this cv, please try again" });
   }
 
   try {
@@ -123,24 +73,54 @@ export const createEducation = async (req, res, next) => {
     await sess.commitTransaction();
   } catch (err) {
     console.log(err);
-    return res.status(400).json({ errorMessage: "Fail." });
+    return res.status(402).json({ errorMessage: "Fail." });
   }
-
-  res.status(201).json({ cv: createdEducation });
+  res.status(201).json({ cv: createdProfile });
 };
 
-export const createProject = async (req, res, next) => {
+export const updateProfile = async (req, res, next) => {
   const {
-    cvId,
-    projectName,
-    projectDescription,
+    firstname,
+    lastname,
+    dob,
+    email,
+    address,
+    phone
   } = req.body;
 
-  const createdProject = new Project({
-    cvId,
-    projectName,
-    projectDescription,
-  });
+  const profileId = req.params.profileId;
+
+  let profile;
+  try {
+    profile = await Profile.findById(profileId);
+  } catch {
+    return res.status(400).json({ errorMessage: "Some thing went wrong, please try again" });
+  }
+
+  profile.firstname = firstname;
+  profile.lastname = lastname;
+  profile.dob = dob;
+  profile.email = email;
+  profile.address = address;
+  profile.phone = phone;
+
+  if (!profileId) {
+    return res.status(401).json({ errorMessage: "Can not find this profile, please try again" });
+  }
+
+  try {
+    await profile.save();
+  } catch (err) {
+    console.log(err);
+    return res.status(402).json({ errorMessage: "Fail." });
+  }
+  res.status(201).json({ profile: profile.toObject({ getters: true }) });
+};
+
+export const createEducation = async (req, res, next) => {
+  const { cvId } = req.params;
+
+  const createdEducation = new Education({ cvId });
 
   let cv;
   try {
@@ -150,7 +130,7 @@ export const createProject = async (req, res, next) => {
   }
 
   if (!cv) {
-    return res.status(400).json({ errorMessage: "Can not find this cv, please try again" });
+    return res.status(401).json({ errorMessage: "Can not find this cv, please try again" });
   }
 
   try {
@@ -162,28 +142,41 @@ export const createProject = async (req, res, next) => {
     await sess.commitTransaction();
   } catch (err) {
     console.log(err);
-    return res.status(400).json({ errorMessage: "Fail." });
+    return res.status(402).json({ errorMessage: "Fail." });
   }
-
-  res.status(201).json({ cv: createdProject });
+  res.status(201).json({ cv: createdEducation });
 };
 
-export const createExperience = async (req, res, next) => {
-  const {
-    cvId,
-    companyName,
-    duration,
-    position,
-    expDescription,
-  } = req.body;
+export const updateEducation = async (req, res, next) => {
+  const edu = req.body.education;
 
-  const createdExperience = new Experience({
-    cvId,
-    companyName,
-    duration,
-    position,
-    expDescription,
-  });
+  const educationId = req.params.eduId;
+  let education;
+  try {
+    education = await Education.findById(educationId);
+  } catch {
+    return res.status(400).json({ errorMessage: "Some thing went wrong, please try again" });
+  }
+
+  education.education = edu;
+
+  if (!educationId) {
+    return res.status(401).json({ errorMessage: "Can not find this education, please try again" });
+  }
+
+  try {
+    await education.save();
+  } catch (err) {
+    console.log(err);
+    return res.status(402).json({ errorMessage: "Fail." });
+  }
+  res.status(201).json({ education: education.toObject({ getters: true }) });
+};
+
+export const createProject = async (req, res, next) => {
+  const { cvId } = req.params;
+
+  const createdProject = new Project({ cvId });
 
   let cv;
   try {
@@ -193,7 +186,7 @@ export const createExperience = async (req, res, next) => {
   }
 
   if (!cv) {
-    return res.status(400).json({ errorMessage: "Can not find this cv, please try again" });
+    return res.status(401).json({ errorMessage: "Can not find this cv, please try again" });
   }
 
   try {
@@ -205,24 +198,46 @@ export const createExperience = async (req, res, next) => {
     await sess.commitTransaction();
   } catch (err) {
     console.log(err);
-    return res.status(400).json({ errorMessage: "Fail." });
+    return res.status(402).json({ errorMessage: "Fail." });
   }
-
-  res.status(201).json({ cv: createdExperience });
+  res.status(201).json({ cv: createdProject });
 };
 
-export const createExtras = async (req, res, next) => {
+export const updateProject = async (req, res, next) => {
   const {
-    cvId,
-    skill,
-    interest,
+    projectName,
+    projectDescription,
   } = req.body;
 
-  const createdExtras = new Extra({
-    cvId,
-    skill,
-    interest,
-  });
+  const projectId = req.params.projectId;
+
+  let project;
+  try {
+    project = await Project.findById(projectId);
+  } catch {
+    return res.status(400).json({ errorMessage: "Some thing went wrong, please try again" });
+  }
+
+  project.projectName = projectName;
+  project.projectDescription = projectDescription;
+
+  if (!projectId) {
+    return res.status(401).json({ errorMessage: "Can not find this project, please try again" });
+  }
+
+  try {
+    await project.save();
+  } catch (err) {
+    console.log(err);
+    return res.status(402).json({ errorMessage: "Fail." });
+  }
+  res.status(201).json({ project: project.toObject({ getters: true }) });
+};
+
+export const createExperience = async (req, res, next) => {
+  const { cvId } = req.params;
+
+  const createdExperience = new Experience({ cvId });
 
   let cv;
   try {
@@ -232,22 +247,115 @@ export const createExtras = async (req, res, next) => {
   }
 
   if (!cv) {
-    return res.status(400).json({ errorMessage: "Can not find this cv, please try again" });
+    return res.status(401).json({ errorMessage: "Can not find this cv, please try again" });
   }
 
   try {
     const sess = await mongoose.startSession();
     sess.startTransaction();
-    await createdExtras.save({ session: sess });
-    cv.extra.push(createdExtras);
+    await createdExtra.save({ session: sess });
+    cv.extra.push(createdExtra);
     await cv.save({ sess: sess });
     await sess.commitTransaction();
   } catch (err) {
     console.log(err);
-    return res.status(400).json({ errorMessage: "Fail." });
+    return res.status(402).json({ errorMessage: "Fail." });
+  }
+  res.status(201).json({ cv: createdExperience });
+};
+
+export const updateExperience = async (req, res, next) => {
+  const {
+    companyName,
+    duration,
+    position,
+    expDescription,
+  } = req.body;
+
+  const experienceId = req.params.expId;
+
+  let experience;
+  try {
+    experience = await Experience.findById(experienceId);
+  } catch {
+    return res.status(400).json({ errorMessage: "Some thing went wrong, please try again" });
   }
 
-  res.status(201).json({ cv: createdExtras });
+  experience.companyName = companyName;
+  experience.duration = duration;
+  experience.position = position;
+  experience.expDescription = expDescription;
+
+  if (!experienceId) {
+    return res.status(401).json({ errorMessage: "Can not find this experience, please try again" });
+  }
+
+  try {
+    await experience.save();
+  } catch (err) {
+    console.log(err);
+    return res.status(402).json({ errorMessage: "Fail." });
+  }
+  res.status(201).json({ experience: experience.toObject({ getters: true }) });
+};
+
+export const createExtra = async (req, res, next) => {
+  const { cvId } = req.params;
+
+  const createdExtra = new Extra({ cvId });
+
+  let extra;
+  try {
+    extra = await Extra.findById(extraId);
+  } catch {
+    return res.status(400).json({ errorMessage: "Some thing went wrong, please try again" });
+  }
+
+  if (!cv) {
+    return res.status(401).json({ errorMessage: "Can not find this cv, please try again" });
+  }
+
+  try {
+    const sess = await mongoose.startSession();
+    sess.startTransaction();
+    await createdExtra.save({ session: sess });
+    cv.extra.push(createdExtra);
+    await cv.save({ sess: sess });
+    await sess.commitTransaction();
+  } catch (err) {
+    console.log(err);
+    return res.status(402).json({ errorMessage: "Fail." });
+  }
+  res.status(201).json({ cv: createdExtra });
+};
+
+export const updateExtra = async (req, res, next) => {
+  const {
+    addInfor,
+  } = req.body;
+
+  const extraId = req.params.extraId;
+
+  let extra;
+  try {
+    extra = await Extra.findById(extraId);
+  } catch {
+    return res.status(400).json({ errorMessage: "Some thing went wrong, please try again" });
+  }
+
+  extra.addInfor = addInfor;
+
+  if (!extraId) {
+    return res.status(401).json({ errorMessage: "Can not find this experience, please try again" });
+  }
+
+  try {
+    await extra.save();
+  } catch (err) {
+    console.log(err);
+    return res.status(402).json({ errorMessage: "Fail." });
+  }
+  res.status(201).json({ extra: extra.toObject({ getters: true }) });
 };
 
 export const updateCv = async (req, res, next) => {
